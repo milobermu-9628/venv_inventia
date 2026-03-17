30ba99ca6355b5f3289b619d954d8d3dc72f6818
milobermu-9628 <milobermu@gmail.com>
2026-03-16 19:01:00 -0500

    conectar base de datos

--- database.py
+++ database.py
@@ -11,9 +11,8 @@ DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_HOST = os.getenv("DB_HOST")
 DB_NAME = os.getenv("DB_NAME")
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:@localhost/INVENTIA"
-#SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:@localhost/INVENTIA" Cambiar a la que tengo en railway
-
+#SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:@localhost/INVENTIA"
+SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://root:fVcJNNcinwSfNeXDDBKrUFPUymdNxfTQ:@trolley.proxy.rlwy.net:56365/railway"
 engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
